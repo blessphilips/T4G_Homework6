823a82c7ab2d7b4ed5d55b643a68b49e9d6811b0
blessphilips <blessphilips>
2024-07-17 08:09:28 -0700

    styled the contact us page

--- src/index.js
+++ src/index.js
@@ -8,13 +8,6 @@ function promptName(){
 let product=document.getElementById("product");
 product.addEventListener("mouseenter", promptName);
 
-// alerting the links
-function alertlink(){
-alert("https://www.bing.com/search?q=african+spices&FORM=HDRSC1");
-}
-
-let spices=document.querySelector("#Spices");//added a click event to the spice img.
-spices.addEventListener("click", alertlink);
 
 //this block is loading more images.
 function loadMore() { //This function executes when the button is dbl clicked
@@ -26,3 +19,38 @@ load.style.display = "none";
 
 let load = document.getElementById("load"); //select button you want to add event listener to
 load.addEventListener("dblclick", loadMore); //Add an event listener to the button you have selected above
+
+// this block is hiding the images
+function loadless(){//adding loadless to this block
+let hideimage=document.getElementById("set");
+hideimage.style.display ="none";
+}
+let hide=document.getElementById("hide");
+hide.addEventListener("dblclick",loadless);
+
+
+// alerting the external links to African Spice.
+function alertlink(){
+    alert("https://www.bing.com/search?q=african+spices&FORM=HDRSC1");
+    }
+    
+    let spices=document.querySelector("#Spices");//added a click event to the spice img.
+    spices.addEventListener("click", alertlink);
+    
+    function alertImageChange(){//spice image should decrease when the mouse enter.
+    let imagechange=document.getElementById("Spices");
+    imagechange.style.width= "101%";
+    // imagechange.style.animationDuration="800ms";
+    // imagechange.style.height="300px";
+    }
+    let change=document.getElementById("Spices");
+    change.addEventListener("mouseenter", alertImageChange);
+    
+    function alertImageDecrease(){//spices should go back to it"s origin when clicked
+        let decrease=document.getElementById("Spices");
+        decrease.style.width="";
+        decrease.style.height="";
+    }
+    let imageDecrease=document.getElementById("Spices");
+    imageDecrease.addEventListener("mouseleave", alertImageDecrease);
+    
