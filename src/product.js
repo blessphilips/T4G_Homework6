9eb9e0d0cee13fd6f41ccf660b023dec27560ac9
blessphilips <blessphilips>
2024-07-25 09:57:57 -0700

    adding changes

---
+++ src/product.js
@@ -0,0 +1,173 @@
+function change(){
+    let man=document.getElementById("show");
+    man.style.display="inline";
+    let hey=document.getElementById("more");
+    hey.style.display="none";
+    let Hey=document.getElementById("lessone");
+    Hey.style.display="inline";
+}
+
+let mine =document.getElementById("more");
+mine.addEventListener("click",change);
+
+function size(){
+    let who=document.getElementById("show");
+    who.style.display="none";
+    let Who=document.getElementById("more");
+    Who.style.display="inline";
+    let Whom=document.getElementById("lessone");
+    Whom.style.display="none";
+
+}
+
+let me =document.getElementById("lessone");
+me.addEventListener("click",size);
+
+
+
+
+function Change(){
+    let Man=document.getElementById("move");
+    Man.style.display="inline";
+    let heyyo=document.getElementById("do");
+    heyyo.style.display="none";
+    let Heyya=document.getElementById("lesstwo");
+    Heyya.style.display="inline";
+}
+
+let Mine =document.getElementById("do");
+Mine.addEventListener("click",Change);
+
+
+
+function Size(){
+    let who=document.getElementById("move");
+    who.style.display="none";
+    let Who=document.getElementById("do");
+    Who.style.display="inline";
+    let Whom=document.getElementById("lesstwo");
+    Whom.style.display="none";
+
+}
+
+let Me =document.getElementById("lesstwo");
+Me.addEventListener("click",Size);
+
+
+
+function hello(){
+    let Man=document.getElementById("readone");
+    Man.style.display="inline";
+    let Heyyo=document.getElementById("moreone");
+    Heyyo.style.display="none";
+    let heyya=document.getElementById("lessthree");
+    heyya.style.display="inline";
+}
+
+let what =document.getElementById("moreone");
+what.addEventListener("click",hello);
+
+
+
+
+function so(){
+    let Whom=document.getElementById("readone");
+    Whom.style.display="none";
+    let Whot=document.getElementById("moreone");
+    Whot.style.display="inline";
+    let Whome=document.getElementById("lessthree");
+    Whome.style.display="none";
+
+}
+
+let Meme =document.getElementById("lessthree");
+Meme.addEventListener("click",so);
+
+
+
+
+function say(){
+    let Men=document.getElementById("soup");
+    Men.style.display="inline";
+    let sit=document.getElementById("moreseven");
+    sit.style.display="none";
+    let sat=document.getElementById("lessfour");
+    sat.style.display="inline";
+}
+
+let where =document.getElementById("moreseven");
+where.addEventListener("click",say);
+
+
+function sot(){
+    let hom=document.getElementById("soup");
+    hom.style.display="none";
+    let hot=document.getElementById("moreseven");
+    hot.style.display="inline";
+    let home=document.getElementById("lessfour");
+    home.style.display="none";
+
+}
+
+let Mem =document.getElementById("lessfour");
+Mem.addEventListener("click",sot);
+
+
+
+function saw(){
+    let Mene=document.getElementById("sisters");
+    Mene.style.display="inline";
+    let it=document.getElementById("moretwo");
+    it.style.display="none";
+    let at=document.getElementById("lessfive");
+    at.style.display="inline";
+}
+
+let here =document.getElementById("moretwo");
+here.addEventListener("click",saw);
+
+
+
+function ot(){
+    let chom=document.getElementById("sisters");
+    chom.style.display="none";
+    let hote=document.getElementById("moretwo");
+    hote.style.display="inline";
+    let homes=document.getElementById("lessfive");
+    homes.style.display="none";
+
+}
+
+let make =document.getElementById("lessfive");
+make.addEventListener("click",ot);
+
+
+
+
+
+function wait(){
+    let Make=document.getElementById("the");
+    Make.style.display="inline";
+    let lit=document.getElementById("morethree");
+    lit.style.display="none";
+    let lat=document.getElementById("lesssix");
+    lat.style.display="inline";
+}
+
+let keep =document.getElementById("morethree");
+keep.addEventListener("click",wait);
+
+
+
+function to(){
+    let chom=document.getElementById("the");
+    chom.style.display="none";
+    let hote=document.getElementById("morethree");
+    hote.style.display="inline";
+    let homes=document.getElementById("lesssix");
+    homes.style.display="none";
+
+}
+
+let Kake =document.getElementById("lesssix");
+Kake.addEventListener("click",to);
